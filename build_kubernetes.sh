4a9743a6b544d4a50b82bee7d68825a183ea25f9
Robert Deusser <robert.deusser@nextgearcapital.com>
2016-04-11 14:53:46 -0400

    Initial commit

---
+++ build_kubernetes.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+K8S_VERSION=${K8S_VERSION:-1.2.2}
+
+curl -L https://storage.googleapis.com/kubernetes-release/release/v$K8S_VERSION/kubernetes.tar.gz | tar xvz
+
+tar xfvz kubernetes/server/kubernetes-server-linux-amd64.tar.gz
